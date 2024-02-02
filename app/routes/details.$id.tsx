ac2c8525fe1e0bdfaa700504071347b8bd7e55fe
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-02 03:06:47 +0000

    feat: fetch movie by id on details page

--- app/routes/details.$id.tsx
+++ app/routes/details.$id.tsx
@@ -1,7 +1,25 @@
-import { useParams } from "@remix-run/react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const apiKey = process.env.OMDB_API_KEY;
+  const response = await fetch(
+    `https://www.omdbapi.com/?i=${params.id}&apikey=${apiKey}`
+  );
+  const movie = await response.json();
+  invariant(movie, "Movie not found");
+  return json(movie);
+}
 
 export default function Details() {
-  const params = useParams();
+  const movie = useLoaderData<typeof loader>();
 
-  return <div>Details from movie: {params.id}</div>;
+  return (
+    <div>
+      <h1>{movie.Title}</h1>
+      <img src={movie.Poster} alt={movie.Title} />
+      <p>{movie.Plot}</p>
+    </div>
+  );
 }
