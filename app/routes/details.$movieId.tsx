042c9685fe7df9ce416509f6d12b9da602ea97e1
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-04 19:03:05 +0000

    feat: add a form to handle fav/unfav action

--- app/routes/details.$movieId.tsx
+++ app/routes/details.$movieId.tsx
@@ -1,24 +1,54 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { useFetcher, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
+import { userFavorites } from "../cookies.server";
 
-export async function loader({ params }: LoaderFunctionArgs) {
+const DEFAULT_COOKIE_VALUE = { favorites: new Set() };
+
+async function getCookieValue(
+  request: LoaderFunctionArgs["request"] | ActionFunctionArgs["request"]
+) {
+  const cookieHeader = request.headers.get("Cookie");
+  return (await userFavorites.parse(cookieHeader)) ?? DEFAULT_COOKIE_VALUE;
+}
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
   const apiKey = process.env.OMDB_API_KEY;
   const response = await fetch(
     `https://www.omdbapi.com/?i=${params.movieId}&apikey=${apiKey}`
   );
   const movie = await response.json();
   invariant(movie, "Movie not found");
-  return json(movie);
+
+  const cookie = await getCookieValue(request);
+  invariant(cookie.favorites, "Missing favorites in cookie");
+
+  return json({ movie, favorite: cookie.favorites.has(params.movieId) });
+}
+
+export async function action({ params, request }: ActionFunctionArgs) {
+  invariant(params.movieId, "Missing movieId param");
+
+  const cookie = await getCookieValue(request);
+  invariant(cookie.favorites, "Missing favorites in cookie");
+
+  const formData = await request.formData();
+  if (formData.get("favorite") === "false") {
+    cookie.favorites.delete(params.movieId);
+  } else {
+    cookie.favorites.add(params.movieId);
+  }
+
+  return json({ ok: true });
 }
 
 export default function Details() {
-  const movie = useLoaderData<typeof loader>();
+  const { movie, favorite } = useLoaderData<typeof loader>();
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: 12 }}>
       <h1>{movie.Title}</h1>
-      <button style={{ width: 80 }}>Favorite</button>
+      <Favorite movie={{ favorite }} />
       <div style={{ display: "flex", gap: 8 }}>
         <div style={{ width: 280, height: 420 }}>
           <img
@@ -35,5 +65,22 @@ export default function Details() {
         <p style={{ margin: 0, maxWidth: 560 }}>{movie.Plot}</p>
       </div>
     </div>
+  );
+}
+
+function Favorite({ movie }: { movie: { favorite: boolean } }) {
+  const fetcher = useFetcher();
+  const favorite = movie.favorite;
+
+  return (
+    <fetcher.Form method="post">
+      <button
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+        name="favorite"
+        value={favorite ? "false" : "true"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </fetcher.Form>
   );
 }
