b1cc2ee38e449d28928b7875bf5251475729100d
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-05 15:43:17 +0000

    style: add margin to main element

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -25,7 +26,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <main className="max-w-screen-lg mx-auto py-8">
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
