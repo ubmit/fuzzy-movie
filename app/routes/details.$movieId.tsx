ea4317bb31752b712d47b036bda1e6538646ef00
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-09 21:52:37 +0000

    style: polish the favorite button

--- app/routes/details.$movieId.tsx
+++ app/routes/details.$movieId.tsx
@@ -3,6 +3,7 @@ import { useFetcher, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { userFavorites } from "../cookies.server";
 import { getMovieById } from "~/data/get-movie-by-id";
+import { SVGProps } from "react";
 
 type Cookie = { favorites: string[] };
 
@@ -84,13 +85,41 @@ function Favorite({
   return (
     <fetcher.Form method="post" className={className}>
       <button
-        className="p-2"
+        className="p-2 mt-2 mr-2 rounded-md bg-gray-200/60 backdrop-blur-md shadow-md"
         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
         value={favorite ? "false" : "true"}
       >
-        {favorite ? "★" : "☆"}
+        <HeartIcon
+          className={[
+            "w-6 h-6 stroke-2",
+            favorite
+              ? "stroke-red-600 fill-red-600"
+              : "stroke-gray-500 fill-none",
+          ]
+            .filter(Boolean)
+            .join(" ")}
+        />
       </button>
     </fetcher.Form>
+  );
+}
+
+function HeartIcon({ className }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2c-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+    </svg>
   );
 }
