89c4d6988607f32b43c080e6c03b2f0af79339f9
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-10 17:28:25 +0000

    refactor: extract the HeartIcon comp

--- app/routes/details.$movieId/route.tsx
+++ app/routes/details.$movieId/route.tsx
@@ -3,7 +3,7 @@ import { useFetcher, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { userFavorites } from "../../cookies.server";
 import { getMovieById } from "~/data/get-movie-by-id";
-import { SVGProps } from "react";
+import { HeartIcon } from "./heart-icon";
 
 type Cookie = { favorites: string[] };
 
@@ -102,24 +102,5 @@ function Favorite({
         />
       </button>
     </fetcher.Form>
-  );
-}
-
-function HeartIcon({ className }: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      className={className}
-    >
-      <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2c-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-    </svg>
   );
 }
