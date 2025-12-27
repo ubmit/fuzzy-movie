b6d191a95066b7acdfb822a024b258f58b95fbd6
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2025-12-27 19:07:43 +0000

    fix: raise an error if getMovieById response isn't ok

--- app/data/get-movie-by-id.ts
+++ app/data/get-movie-by-id.ts
@@ -4,5 +4,12 @@ export async function getMovieById(movieId: string) {
   const response = await fetch(
     `https://www.omdbapi.com/?i=${movieId}&plot=full&apikey=${env.OMDB_API_KEY}`
   );
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch movie with id "${movieId}": ${response.status} ${response.statusText}`
+    );
+  }
+
   return response.json();
 }
